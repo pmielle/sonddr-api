d8210a2ed5661912e095549283a8cd288c1b7d50
Paul Mielle <pm.mielle@gmail.com>
2022-07-30 16:29:52 +0200

    insert goals at init

--- database/init.sh
+++ database/init.sh
@@ -1,10 +1,23 @@
 mongo <<EOF
 
+// target the main database
+// ------------------------
 db = db.getSiblingDB('$MONGO_DATABASE')
 
+// create the collections
+// ----------------------
 db.createCollection("goals")
 db.createCollection("ideas")
 
+// init the goals
+// --------------
+db.goals.insertMany([
+	{name: "No poverty"},
+	{name: "Quality education"}
+])
+
+// create the web user that is used by the api
+// -------------------------------------------
 db.createUser({
     user: '$MONGO_WEB_USER',
     pwd: '$MONGO_WEB_PASSWORD',
