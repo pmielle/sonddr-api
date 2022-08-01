bb99b12f77417df7050807781c93bac8c73f3dca
Paul Mielle <pm.mielle@gmail.com>
2022-08-01 22:26:36 +0200

    post idea and (badly) parse its date

--- api/src/routes/ideas.js
+++ api/src/routes/ideas.js
@@ -1,10 +1,13 @@
-import { getCollection, getDocument } from "../database/database.js"
+import { addToCollection, getCollection, getDocument } from "../database/database.js"
 
 export function addIdeasRoutes(app) {
 
     const endpoint = "/ideas"
     const databaseCollectionId = "ideas"
 
+
+    // GET
+    // ---
     app.get(endpoint, async (req, res, next) => {
         try {
             res.json(await getCollection(databaseCollectionId))
@@ -15,6 +18,17 @@ export function addIdeasRoutes(app) {
         try {
             res.json(await getDocument(databaseCollectionId, req.params.id))
         } catch (err) { next(err) }
-    }) 
+    })
+
+    // POST
+    // ----
+    app.post(endpoint, async (req, res, next) => {
+        // convert string timestamp into date
+        req.body.date = new Date(req.body.date)  // <-- FIXME: never fails...
+        try {
+            // post
+            res.json(await addToCollection(databaseCollectionId, req.body))
+        } catch (err) { next(err) }
+    })
 
 }
