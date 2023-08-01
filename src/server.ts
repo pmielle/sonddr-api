8ee0ce85a36a84c95fe1a19dc7a60659f50bb6a9
Paul Mielle <pm.mielle@gmail.com>
2023-08-01 10:57:56 +0200

    express init

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello world!");
+import cors from "cors";
+import express from "express";
+
+const port = 3000;
+const app = express();
+app.use(cors({origin: "http://localhost:4200"}));
+
+app.get('/', (req, res) => {
+    res.json(null);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
