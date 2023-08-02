258b9280e03ee24df446165a876b995bd9fb77d8
Paul Mielle <pm.mielle@gmail.com>
2023-08-02 20:39:53 +0200

    handle mongoid

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,6 @@
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient, ObjectId, BSON } from "mongodb";
 import { Doc, NotFoundError } from "./types";
+import crypto from "crypto";
 
 const uri = "mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=rs0&readPreference=primary&ssl=false";
 const client = new MongoClient(uri);
@@ -8,7 +9,8 @@ const db = client.db("sonddr");
 export async function getDocument(path: string): Promise<Doc> {
     const [collId, docId] = _parseDocumentPath(path);
     const coll = db.collection(collId);
-    const query = { _id: new ObjectId(docId) };
+    const query = { _id: _makeMongoId(docId) };
+    console.log(query);
     let dbDoc = await coll.findOne(query);
     if (!dbDoc) {
         throw new NotFoundError();
@@ -20,6 +22,23 @@ export async function getDocument(path: string): Promise<Doc> {
 
 // private
 // ----------------------------------------------
+// this cannot be changed recklessly because already inserted documents won't be able to be fetched anymore without a patch
+function _makeMongoId(id: string): ObjectId {
+    const reqLength = 24;
+    let objectId: ObjectId;
+    try {
+        const hash = crypto.createHash('md5').update(id).digest('hex');
+        const hashWithValidLength = hash.slice(0, reqLength);
+        objectId = new ObjectId(hashWithValidLength);
+    } catch(err) {
+        if (err instanceof BSON.BSONError) {
+            throw new Error(`Failed to convert ${id} into a mongo ObjectId: ${err}`);
+        }
+        throw err;
+    }
+    return objectId;
+}
+
 function _parseDocumentPath(path: string): [string, string] {  // returns collection and document ids
     const splitResult = path.split("/").filter(x => x.length > 0);
     if (splitResult.length != 2) {
