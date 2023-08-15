efaf6ea0f77db120597e2a11146f5b8617959283
Paul Mielle <pm.mielle@gmail.com>
2023-08-15 17:44:54 +0200

    db init + tests routes

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { MongoClient, ObjectId, BSON } from "mongodb";
+import { MongoClient, ObjectId, BSON, WithId } from "mongodb";
 import { Doc, NotFoundError } from "./types";
 import crypto from "crypto";
 
@@ -6,29 +6,106 @@ const uri = "mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaS
 const client = new MongoClient(uri);
 const db = client.db("sonddr");
 
-export async function getDocument(path: string): Promise<Doc> {
+export async function getDocument<T extends Doc>(path: string): Promise<T> {
     const [collId, docId] = _parseDocumentPath(path);
     const coll = db.collection(collId);
     const query = { _id: _makeMongoId(docId) };
-    let dbDoc = await coll.findOne(query);
+    const dbDoc = await coll.findOne(query);
     if (!dbDoc) {
         throw new NotFoundError();
     }
-    let {_id, ...obj} = dbDoc;
-    obj.id = dbDoc._id.toString();
-    return obj as any;  // compiler is dumb
+    const doc = _replaceObjectIdWithId(dbDoc);
+    return doc as any;  // wtf typescript?
+}
+
+export async function getDocuments<T extends Doc>(path: string): Promise<T[]> {
+    const coll = db.collection(path);
+    const dbDocs = await coll.find().toArray();
+    const docs = dbDocs.map(_replaceObjectIdWithId);
+    return docs as any;  // wtf typescript?
+}
+
+export async function postDocument(path: string, payload: object): Promise<string> {
+    // do not allow "id" in payload
+    if ("id" in payload) {
+        throw new Error(`id in POST payload is not allowed: use PUT instead`);
+    }
+    const coll = db.collection(path);
+    const result = await coll.insertOne(payload);
+    const id = result.insertedId;
+    return id.toString();
+}
+
+export async function putDocument(path: string, payload: object): Promise<string> {
+    const [collId, docId] = _parseDocumentPath(path);
+    // handle 2 types of payloads: with or without an "id" field
+    let dbDoc: WithId<BSON.Document>;
+    if ("id" in payload) {
+        if (payload.id !== docId) {
+            throw new Error(`Payload id does not match endpoint id: ${payload.id} != ${docId}`);
+        }
+        dbDoc = _replaceIdWithObjectId(payload as Doc);
+    } else {
+        dbDoc = { 
+            ...payload,
+            _id: _makeMongoId(docId), 
+        };
+    }
+    const coll = db.collection(collId);
+    const result = await coll.insertOne(dbDoc);
+    const _id = result.insertedId;
+    return _id.toString();
+}
+
+export async function deleteDocument(path: string): Promise<void> {
+    const [collId, docId] = _parseDocumentPath(path);
+    const coll = db.collection(collId);
+    const query = { _id: _makeMongoId(docId) };
+    let result = await coll.deleteOne(query);
+    if (result.deletedCount == 0) {
+        throw new Error("0 documents were deleted");
+    }
+    return;
+}
+
+export async function patchDocument<T extends Doc>(path: string, payload: Partial<T>): Promise<void> {
+    const [collId, docId] = _parseDocumentPath(path);
+    // do not allow "id" update
+    if ("id" in payload) {
+        throw new Error(`id in PATCH payload is not allowed`);
+    }
+    const coll = db.collection(collId);
+    const query = { _id: _makeMongoId(docId) };
+    await coll.updateOne(query, {$set: payload});
+    return;
 }
 
 // private
 // ----------------------------------------------
+function _replaceObjectIdWithId(dbDoc: WithId<BSON.Document>): Doc {
+    const {_id, ...obj} = dbDoc;
+    obj.id = dbDoc._id.toString();
+    return obj as any;  // wtf typescript?
+}
+
+function _replaceIdWithObjectId(doc: Doc): WithId<BSON.Document> {
+    const {id, ...dbDoc} = doc;
+    dbDoc._id = _makeMongoId(id);
+    return dbDoc as any;  // wtf typescript?
+}
+
 // this cannot be changed recklessly because already inserted documents won't be able to be fetched anymore without a patch
 function _makeMongoId(id: string): ObjectId {
     const reqLength = 24;
     let objectId: ObjectId;
     try {
-        const hash = crypto.createHash('md5').update(id).digest('hex');
-        const hashWithValidLength = hash.slice(0, reqLength);
-        objectId = new ObjectId(hashWithValidLength);
+        if (id.length == reqLength) {
+            objectId = new ObjectId(id);
+        } else {
+            const hash = crypto.createHash('md5').update(id).digest('hex');
+            const hashWithValidLength = hash.slice(0, reqLength);
+            objectId = new ObjectId(hashWithValidLength);
+        }
     } catch(err) {
         if (err instanceof BSON.BSONError) {
             throw new Error(`Failed to convert ${id} into a mongo ObjectId: ${err}`);
@@ -43,5 +120,5 @@ function _parseDocumentPath(path: string): [string, string] {  // returns collec
     if (splitResult.length != 2) {
         throw new Error(`path '${path}' should yield 2 non-empty elements when split to '/'`);
     }
-    return splitResult as any;  // compiler is dumb
+    return splitResult as any;  // wtf typescript?
 }
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,110 @@
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import { NotFoundError } from "./types";
-import { getDocument } from "./database";
+import { deleteDocument, getDocument, getDocuments, patchDocument, postDocument, putDocument } from "./database";
+import chalk from "chalk";
+
 
 const port = 3000;
 const app = express();
-app.use(cors({origin: "http://localhost:4200"}));
+app.use(express.json());  // otherwise req.body is undefined
+app.use(cors({origin: "http://localhost:4200"}));  // otherwise can't be reached by front
 
-app.get('/goals/:id', async (req, res, next) => {
+
+// routes
+// ----------------------------------------------
+app.get('/tests/:id', async (req, res, next) => {
     try {
-        const doc = await getDocument(req.path);
+        const doc = await getDocument(_getReqPath(req));
         res.json(doc);
     } catch(err) { 
         next(err); 
     }
 });
 
+app.get('/tests', async (req, res, next) => {
+    try {
+        const docs = await getDocuments(_getReqPath(req));
+        res.json(docs);
+    } catch(err) { 
+        next(err); 
+    }
+});
+
+app.post('/tests', async (req, res, next) => {
+    try {
+        const id = await postDocument(_getReqPath(req), req.body);
+        res.json(id);
+    } catch(err) { 
+        next(err); 
+    }
+});
+
+app.put('/tests/:id', async (req, res, next) => {
+    try {
+        const id = await putDocument(_getReqPath(req), req.body);
+        res.json(id);
+    } catch(err) { 
+        next(err); 
+    }
+});
+
+app.patch('/tests/:id', async (req, res, next) => {
+    try {
+        await patchDocument(_getReqPath(req), req.body);
+        res.status(200).send();
+    } catch(err) { 
+        next(err); 
+    }
+});
+
+app.delete('/tests/:id', async (req, res, next) => {
+    try {
+        await deleteDocument(_getReqPath(req));
+        res.status(200).send();
+    } catch(err) { 
+        next(err); 
+    }
+});
+
+
+// error handling
+// ----------------------------------------------
 app.use(_errorHandler);
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+    console.log(`\n\n`);
 });
 
 
 // private
 // ----------------------------------------------
 function _errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    console.error(chalk.red(`⚠️ ⚠️ ⚠️ ERROR AT ${new Date()}`));
+    console.error(`------------------------------`);
+    console.error(`REQUEST DETAILS`);
+    console.error(`------------------------------`);
+    console.error(`url    : ${req.originalUrl}`);
+    console.error(`body   : ${JSON.stringify(req.body)}`);
+    console.error(chalk.gray(`headers: ${JSON.stringify(req.headers)}`));
+    console.error(`------------------------------`);
+    console.error(`ERROR MESSAGE`);
+    console.error(`------------------------------`);
     console.error(err);
+    console.error(`\n\n`);
     if (err instanceof NotFoundError) {
         res.status(404).send();
     } else {
         res.status(500).send();
     }
+}
+
+function _getReqPath(req: Request): string {
+    let path = req.path;
+    if (path.charAt(0) == "/") { 
+        path = path.substring(1); 
+    }
+    return path;
 }
