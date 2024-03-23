dd3bf0b237c2cfee16bd7a5cb9a55dd9b075070c
Paul Mielle <pm.mielle@gmail.com>
2024-03-23 15:37:14 +0100

    images names with extension

--- src/uploads.ts
+++ src/uploads.ts
@@ -5,11 +5,31 @@ const b_in_mb = 1048576;
 
 export const multerPath = "uploads";
 
-export const upload = multer({dest: multerPath, limits: {
-	files: 20,
-	fileSize: 50 * b_in_mb,
-} });
+export const upload = multer({
+	storage: multer.diskStorage({
+		destination: multerPath,
+		filename: (req, file, callback) => {
+			const extension = getImageExtension(file);
+			const uniqueSuffix = generateUniqueName();
+			const filename = `${file.fieldname}-${uniqueSuffix}.${extension}`;
+			callback(null, filename)
+		},
+	}),
+	limits: { files: 20, fileSize: 50 * b_in_mb },
+});
 
 export function deleteUpload(path: string) {
 	fs.unlinkSync(`${multerPath}/${path}`);
+}
+
+// private
+// --------------------------------------------
+function generateUniqueName(): string {
+	return Date.now() + '-' + Math.round(Math.random() * 1E9);
+}
+
+function getImageExtension(file: Express.Multer.File): string {
+	const [type, extension] = file.mimetype.split("/");
+	if (type !== "image") { throw new Error(`${file.filename} is not an image; its mimetype is ${file.mimetype}`); };
+	return extension;
 }
